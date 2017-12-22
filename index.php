af6d7f8ed30b7a6c144bd7a4af820ac2668dbfea
p-phornthip <p.phechpanaprai@togetherteam.co.th>
2017-12-22 10:28:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 require_once('send.message.php'); //requires file
 require_once('facebook.php'); //sdk provided by facebook, you need one for tokens and
-$config = array('appId' => '1496661733888719' ,'secret'=>'' );//////////////////////////your application secret
+$config = array('appId' => '433418896723663' ,'secret'=>'882ef62beadf2e40c5ad31f4cd0b58b7' );//////////////////////////your application secret
 $facebook=new Facebook($config);
 if(!$facebook->getUser()){
     $params = array(
   'scope' => 'xmpp_login',
-  'redirect_uri' => 'http://localhost/'
+  'redirect_uri' => 'http://google.com/'
     );
     $url=$facebook->getLoginUrl($params);
     header("location:".$url);
